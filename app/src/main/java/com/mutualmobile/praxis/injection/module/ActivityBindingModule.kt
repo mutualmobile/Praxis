8a6331eae467bd9e1e79dc795f31864579e765e9
Phaniraja Bhandari <phaniraja.bhandari@mutualmobile.com>
2018-03-23 23:50:52 +0530

    Fixed Spelling mistake

--- app/src/main/java/com/mutualmobile/praxis/injection/module/ActivityBindingModule.kt
+++ app/src/main/java/com/mutualmobile/praxis/injection/module/ActivityBindingModule.kt
@@ -1,8 +1,8 @@
 package com.mutualmobile.praxis.injection.module
 
 import android.app.Activity
-import com.mutualmobile.praxis.ui.joke.ShowJokeActivityComponent
 import com.mutualmobile.praxis.ui.joke.ShowJokeActivity
+import com.mutualmobile.praxis.ui.joke.ShowJokeActivityComponent
 import dagger.Binds
 import dagger.Module
 import dagger.android.ActivityKey
@@ -13,13 +13,9 @@ import dagger.multibindings.IntoMap
 @Module
 abstract class ActivityBindingModule {
 
-//  @ActivityScope
-//  @ContributesAndroidInjector(modules = arrayOf(ShowJokeActivityModule::class))
-//  abstract fun showJokeActivity(): ShowJokeActivity
-
   @Binds
   @IntoMap
   @ActivityKey(ShowJokeActivity::class)
-  internal abstract fun bindMainActivity(builder: ShowJokeActivityComponent.Builder): AndroidInjector.Factory<out Activity>
+  internal abstract fun bindShowJokeActivity(builder: ShowJokeActivityComponent.Builder): AndroidInjector.Factory<out Activity>
 
 }
