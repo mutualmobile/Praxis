339c96b8400b1b2fa8344556390feb17b1f461ca
vivekbhalodiya <vivek.bhalodiya@mutualmobile.com>
2018-06-22 15:46:12 +0530

    1. Added README file 2. Added About Us Content

--- app/src/main/java/com/mutualmobile/praxis/ui/home/AboutFragemnt.kt
+++ app/src/main/java/com/mutualmobile/praxis/ui/home/AboutFragemnt.kt
@@ -1,13 +1,15 @@
 package com.mutualmobile.praxis.ui.home
 
-import android.app.Dialog
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleOwner
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mutualmobile.praxis.R
+import com.mutualmobile.praxis.databinding.FragmentAboutBinding
 import dagger.android.DaggerDialogFragment
 
 class AboutFragment : DaggerDialogFragment(), LifecycleOwner {
@@ -20,16 +22,20 @@ class AboutFragment : DaggerDialogFragment(), LifecycleOwner {
       return fragment
     }
   }
+
+  private lateinit var binding: FragmentAboutBinding
   override fun getLifecycle(): Lifecycle {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
   ): View {
-    return inflater.inflate(R.layout.fragment_about, container, false)
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+    binding.mutualMobileWebLink.movementMethod = LinkMovementMethod.getInstance()
+    return binding.root
   }
 
 }
