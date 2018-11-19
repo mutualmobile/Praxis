e941d76c54053e4d34ba754e098faca931d39f74
Phaniraja Bhandari <phaniraja8@gmail.com>
2018-11-19 20:12:50 +0530

    add doc

--- app/src/main/java/com/mutualmobile/praxis/injection/module/ActivityBindingModule.kt
+++ app/src/main/java/com/mutualmobile/praxis/injection/module/ActivityBindingModule.kt
@@ -16,6 +16,9 @@ import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import dagger.android.support.DaggerAppCompatActivity
 
+/**
+ * To create dependencies for a specific activity, don't extend the required activity module with #ActivityModule, instead create a plain module and include #BaseActivityModule in the annotation.
+ */
 @Module
 abstract class ActivityBindingModule {
 
@@ -41,6 +44,7 @@ abstract class ActivityModule<in T : DaggerAppCompatActivity> {
   @ActivityScope
   internal abstract fun bindActivity(activity: T): DaggerAppCompatActivity
 }
+
 
 /**
  * Activity specific common dependencies should be placed here
