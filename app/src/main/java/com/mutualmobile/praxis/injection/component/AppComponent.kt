2b51380c0a04ec7cf51c864410bd62a8473cd842
Suchandrim Sarkar <suchandrim.sarkar@gmail.com>
2021-06-10 13:54:54 +0530

    Moved Data source Implementation to App Module

--- app/src/main/java/com/mutualmobile/praxis/injection/component/AppComponent.kt
+++ app/src/main/java/com/mutualmobile/praxis/injection/component/AppComponent.kt
@@ -3,7 +3,7 @@ package com.mutualmobile.praxis.injection.component
 import android.content.Context
 import com.mutualmobile.praxis.BaseApplication
 import com.mutualmobile.praxis.data.injection.RepositoryModule
-import com.mutualmobile.praxis.data.injection.SourcesModule
+import com.mutualmobile.praxis.injection.module.SourcesModule
 import com.mutualmobile.praxis.data.injection.UseCaseModule
 import com.mutualmobile.praxis.injection.module.ActivityBindingModule
 import com.mutualmobile.praxis.injection.module.AppModule
